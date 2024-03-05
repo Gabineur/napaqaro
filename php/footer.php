82882726a538f14696d8b8de230c8750e4d446b7
{Ǥāƀɍɨēł} <86171976+Gabineur@users.noreply.github.com>
2024-03-05 16:23:58 +0100

    Add files via upload

---
+++ php/footer.php
@@ -0,0 +1,48 @@
+<div class="footer">
+    <div class="footer_content">
+        <div class="footer_logo">
+            <a href="https://napaqaro.000webhostapp.com"><img src="img/logo.png" alt="logo du groupe"></a>
+        </div>
+        <div class="footer_menu">
+            <div class="navfooter">
+                <div class="navfooter_title">Pages de site</div>
+                <ul>
+                    <li class="web_page_napaqaro_1">
+                        <a href="https://napaqaro.000webhostapp.com/histoire/">Histoire</a>
+                    </li>
+                    <li class="web_page_napaqaro_2">
+                        <a href="https://napaqaro.000webhostapp.com/marques/">Marques</a>
+                    </li>
+                    <li class="web_page_napaqaro_3">
+                        <a href="https://napaqaro.000wabhostapp.com/contact/">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="marques">
+                <div class="marques_title">Marques du groupe</div>
+                <ul>
+                    <li class="marques_team_1">
+                        <a href="https://www.buffalo-grill.fr/">Buffalo-grill</a>
+                    </li>
+                    <li class="marques_team_2">
+                        <a href="https://www.courtepaille.com/">Courtepaille</a>
+                    </li>
+                    <li class="marques_team_3">
+                        <a href="https://www.bun-meat-bun.com/">Bun Meat Bun</a>
+                    </li>
+                    <li class="marques_team_4">
+                        <a href="https://www.popeyes.com/">Popeyes</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="reveal_napaqaro">
+                <a href="https://www.napaqaro.fr/">napaqaro - site officiel</a>
+            </div>
+        </div>
+        <div class="white_line">
+            <div class="white_line_content"></div>
+        </div>
+        <div class="copyright">&copy Tous droits non-réservés - NAPAQARO</div>
+        <div class="mentions-legales">ce présent site est réalisé dans le cadre d'un exercice de bts communication</div>
+    </div>
+</div>
---
+++ php/menu.php
@@ -0,0 +1,23 @@
+<div class="navbar">
+            <div class="navbar_content">
+                <div class="logo_img">
+                    <a href="https://napaqaro.000webhostapp.com/"><img src="img/logo.png" alt="logo du groupe"></a>
+                </div>
+                <nav class="navbar_link">
+                    <div class="button_menu_responsive">
+                        <a href="#">Menu</a>
+                    </div>
+                    <ul>
+                        <li class="link_page_1">
+                            <a href="#valeurs">nos valeurs</a>
+                        </li>
+                        <li class="link_page_2">
+                            <a href="#distinctions">nos distinctions</a>
+                        </li>
+                        <li class="link_page_3">
+                            <a href="contact/">contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
